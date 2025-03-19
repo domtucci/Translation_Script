ce75b77762d08c8ba9546c6efd1646d7d7a3b6e4
Dominic Venetucci <dominic.venetucci@dycominc.com>
2025-03-19 09:06:10 -0400

    updated 03.19.2025

--- README.md
+++ README.md
@@ -1,10 +1,48 @@
-# Translation_Script
+# Navigation Language Translation Script
+This program will translate the question dictionaries within a navigation.JSON file and add them to a language configuration file. :smiley:
 
-# append_translation_files
-This python script is used to append translated text to the Spanish/Creole configuration files. The configuration file is used to translate our decision trees within the Rtasq database. It uses a Google Translate API call to translate the question text and any question choice from a list. Note: be sure your internet is running well or else the API calls will fail in the middle of running this program.
+# Inspiration 
+Some users of applications first language is not always English. We were told our decision trees are available in other languages so long that the question exists in that specific language's config file. This is a way to automate the process of adding the questions that exist in our decision trees to the config.JSON file.
 
-# TranslationFILE_to_CSV
-A python scripted used for visual organization of config json spanish and creole files. All it does is put the English and Spanish text into separate columns of a csv file.
+# Installation 
+git clone the repository. 
 
-# Replacement_script
-To finalize the translation config file this program will replace all unicodes with the proper characters. 
+Packages:
+```
+import pandas as pd
+import json
+import PySimpleGUI as sg
+from googletrans import Translator, constants
+import argparse
+from pathlib import Path
+```
+
+You can run the program using PySimpleGUI if license installed. Please see **[run w/ PySimpleGUI](#run-w-pysimplegui)** for instructions on running the program using the GUI and visit **[Licenses](#licenses)** to learn how to acquire PySimpleGUI. 
+
+or 
+
+You can run the program through the command line by assigning the file location in variables. Reference **[run w/ file location](#run-w-file-location)**
+
+## run w/ PySimpleGUI
+Run the program by using: `py append_translation_files.py -v`
+
+Input a JSON file into the GUI window by clicking 'Browse'. The GUI will allow you to browse your computer files. Select the 'navigation_JSON_File.json' you wish to append questions in another language. Select the translation configuration .JSON file. Select a language you wish to translate to. Click 'Submit' and the new CSV file will be in your files where you set the directory in the terminal.
+
+## run w/ file location
+Run the program by using: `py append_translation_files.py -n c:/Users/DV0095/Documents/Python_Projects/translation/nav.json -t c:/Users/DV0095/Documents/Python_Projects/translation/config.json -l Spanish or Creole`
+
+The variable `n` should be assigned the location where the navigation JSON file exists in the file path/directory. 
+The variable `t` should be assigned the location where the language configuration JSON file exists in the file path/directory.
+The variable `l` should be a string of the language you are translating to. Either Spanish or Creole.
+
+The new config JSON file will be in your files where you set the directory in the terminal.
+
+## Licenses
+Submit an Altas Request to get a developer key for the PySimpleGUI library. 'Request New Approved Software'. The library should be an option to select under the approved softwares.
+
+Add developer key: Run the program with PySimpleGUI (**run w/ PySimpleGUI**) and the interface will prompt you to copy and paste the key into a text box. I believe a new key will be needed one calendar year after purchase.
+
+# Visuals
+
+Image of the Spanish config.JSON file
+![alt text](Visual_References/config.png)
--- append_translation_files.py
+++ append_translation_files.py
@@ -1,31 +1,57 @@
 import pandas as pd
 import json
 import PySimpleGUI as sg
-from deep_translator import GoogleTranslator
-import time
-
+from googletrans import Translator, constants
+import argparse
+from pathlib import Path
 
 def main():
-    sg.theme('DarkTeal2')
+    #test_tree=Generator('C://Users//MF0185//Downloads//ECC - Ting (7).csv', run=True)
+    # Add theme to window
+    parser = argparse.ArgumentParser(description="Choose between GUI and command line")
+    parser.add_argument("-v", "--pysimple", action="store_true", help="Use the GUI if you have PySimpleGUI")
+    #parser.add_argument("-c", "--command", type=str, help="Specify the JSON file path for command line mode")
 
-    layout = [[sg.T("")],
-                [sg.Text("Upload the JSON version of the Navigation File: "), sg.Input(key="file_path"), sg.FileBrowse(key="file_path_browse_nav")],
-                [sg.Text('Upload the JSON version of the Translation File: '), sg.Input(key='translation_file'), sg.FileBrowse(key="file_path_browse_translator")],
-                [sg.Text('Please select your target language: '), sg.Listbox(values=['Spanish', 'Creole'], size=(12,1), key='target_language')],
-                [sg.T("")],
-                [sg.Button("Submit", bind_return_key=True), sg.Button('Cancel')]]
+    # File input
+    parser.add_argument("-n", "--file", type=str, help="Path to the navigation JSON file")
+    parser.add_argument("-t", "--tran", type=str, help="Path to the translation JSON file")
 
-    window = sg.Window('Main Menu', layout, size=(800, 250))
+    # Selection of generation type
+    parser.add_argument("-l", "--language", type=str, choices=['Spanish', 'Creole'],
+                        help="Specify the generation type: 'Spanish', or 'Creole'")
 
-    while True:
-        event, values = window.Read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':
-            break
-        elif event == 'Submit':
-            nav_file = values['file_path']
-            translation_file = values['translation_file']
-            target_language = values['target_language'][0]
-            window.close()
+
+    args = parser.parse_args()
+
+    # Read file path
+
+    if args.pysimple:
+        sg.theme('DarkTeal2')
+
+        layout = [[sg.T("")],
+                    [sg.Text("Upload the JSON version of the Navigation File: "), sg.Input(key="file_path"), sg.FileBrowse(key="file_path_browse_nav")],
+                    [sg.Text('Upload the JSON version of the Translation File: '), sg.Input(key='translation_file'), sg.FileBrowse(key="file_path_browse_translator")],
+                    [sg.Text('Please select your target language: '), sg.Listbox(values=['Spanish', 'Creole'], size=(12,1), key='target_language')],
+                    [sg.T("")],
+                    [sg.Button("Submit", bind_return_key=True), sg.Button('Cancel')]]
+
+        window = sg.Window('Main Menu', layout, size=(800, 250))
+
+        while True:
+            event, values = window.Read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+            elif event == 'Submit':
+                nav_file = values['file_path']
+                translation_file = values['translation_file']
+                target_language = values['target_language'][0]
+                window.close()
+
+    elif args.file:           
+        nav_file = Path(args.file)           
+        translation_file = Path(args.tran)
+        target_language = args.language
+
 
     nav_data = json.load(open(nav_file))
     og_translation = json.load(open(translation_file, encoding='UTF-8'))
@@ -33,7 +59,7 @@ def main():
     og_translation['full'] = new_translation
     new_translation_json = json.dumps(og_translation)
 
-    new_file_name = target_language+'_New_1.json'
+    new_file_name = target_language+'_New.json'
     with open(new_file_name, 'w') as f:
         f.write(new_translation_json)
 
@@ -50,8 +76,6 @@ def update_translation_file(nav, original_translation, language):
     for i in range(0, len(sources_list)):
         if type(sources_list[i]) != dict:
             continue
-        # create another if statement that tells the code 'if a digit exists skip it' therefore not translating. Reducing the amount of API calls.
-        # we may be able to regex this
         else:
             if 'list' not in sources_list[i].keys():
                 continue
@@ -73,34 +97,21 @@ def update_translation_file(nav, original_translation, language):
             else:
                 continue
 
+    translator = Translator()
+
     if language == 'Spanish':
         for phrase in missing_phrases:
-            translated_phrase = GoogleTranslator(source = 'auto', target = 'es').translate(text=phrase)
-            translated_phrases.append(translated_phrase)
-            time.sleep(1)
+            translated_phrase = translator.translate(phrase, dest='es')
+            translated_phrases.append(translated_phrase.text)
     elif language == 'Creole':
         for phrase in missing_phrases:
-            translated_phrase = GoogleTranslator(source = 'auto', target = 'ht').translate(text=phrase)
-            translated_phrases.append(translated_phrase)
-            time.sleep(1)
+            translated_phrase = translator.translate(phrase, dest='ht')
+            translated_phrases.append(translated_phrase.text)
 
     translated_dict = {missing_phrases[i]: translated_phrases[i] for i in range(len(missing_phrases))}
     translations.update(translated_dict)
-
+    
     return translations
-
-# def translate_with_retry(translator, phrase, dest, max_retries=3):
-#     retries = 0
-#     while retries < max_retries:
-#         try:
-#             translated = translator.translate(phrase, dest=dest)
-#             return translated.text
-#         except Exception as e:
-#             print(f"Translation failed for '{phrase}'. Retrying...")
-#             retries += 1
-#     return None
-
 
 if __name__=='__main__':
     main()
-
